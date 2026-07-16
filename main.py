52ca33e76a718484d0357b9902428cc27a6010c4
Mister funny computer <tironasaurusnik@gmail.com>
2026-07-16 19:54:58 +0300

    Start make menu

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ window.geometry("400x400")
 content_text = tk.Text(window, wrap = "word")
 content_text.place(x = 0, y = 0, relwidth = 1, relheight = 1)
 
+main_menu = tk.Menu(window)
+window.configure(menu = main_menu)
 
+file_menu = tk.Menu(main_menu, tearoff = 0)
+main_menu.add_cascade(label = "Файл", menu = file_menu)
+
+new_file_icon = tk.PhotoImage(file = "new_file.gif")
+
+
+
+file_menu.add_command(label = "Новый", image = new_file_icon, compound = "left",)
+file_menu.add_command(label = "Открыть")
+file_menu.add_command(label = "Сохранить")
 
 window.mainloop()
