8309f5f50d6f21ec735a77ad23bd9709a195120e
Mister funny computer <tironasaurusnik@gmail.com>
2026-07-23 19:55:31 +0300

    Can open file

--- files.py
+++ files.py
@@ -7,4 +7,8 @@ with open("blank.txt", encoding = "utf-8") as file:
     for line in file:
         print(f"Строчка: {line}")
 
-with open("blank.txt", "w" , encoding = "utf-8") as file:
+#with open("blank.txt", "w" , encoding = "utf-8") as file:
+    #file.write("Так")
+
+with open("blank.txt", "a", encoding = "utf-8") as file:
+    file.write(" \n Уманы умана мы танцуем до утра")
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import tkinter as tk
+import tkinter.filedialog as tfd
 
 window = tk.Tk()
 window.title("Блокнот")
 window.geometry("400x400")
+
+def open_file():
+    file_name = tfd.askopenfilename()
+    with open(file_name, encoding = "utf-8") as file:
+        text = file.read()
+        content_text.delete(1.0, "end")
+        content_text.insert(1.0, text)
+
+
 
 content_text = tk.Text(window, wrap = "word")
 content_text.place(x = 0, y = 0, relwidth = 1, relheight = 1)
@@ -21,7 +31,7 @@ save_file_icon = tk.PhotoImage(file = "save_file.gif")
 
 
 file_menu.add_command(label = "Новый", image = new_file_icon, compound = "left",)
-file_menu.add_command(label = "Открыть", image = open_file_icon, compound = "left",)
+file_menu.add_command(label = "Открыть", image = open_file_icon, compound = "left", command = open_file)
 file_menu.add_command(label = "Сохранить", image = save_file_icon, compound = "left")
 file_menu.add_command(label = "Сохранить как", image = save_file_icon, compound = "left")
 
